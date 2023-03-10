8e8d0a4f72fb4dfb20499fe8149a500339f60df6
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-11 00:33:46 +0100

    started learning abut nstersetion observer

--- script.js
+++ script.js
@@ -72,3 +72,72 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 })
+
+////////////////////////////////////////////////////////
+// ! TABBED COMPONENTS
+const tabsBtns = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) { 
+  const clickedBtn = e.target.closest('.operations__tab');
+
+  if (!clickedBtn) return;
+  
+  tabsBtns.forEach(btn => btn.classList.remove('operations__tab--active'))
+  tabsContent.forEach(content => content.classList.remove('operations__content--active'))
+  clickedBtn.classList.add('operations__tab--active');
+
+  // Activate content area
+  document.querySelector(`.operations__content--${clickedBtn.dataset.tab}`).classList.add('operations__content--active');
+})
+
+/////////////////////////////////////////////////////////
+// ! MENU FADE ANIMATION
+const nav = document.querySelector('.nav');
+
+const handleHover = (e, opacity) => {
+  if (e.target.classList.contains('nav__link')) { 
+    const link = e.target
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link')
+    const logo = link.closest('.nav').querySelector('img')
+
+    siblings.forEach(sibling => {
+      if (sibling !== link) { 
+        sibling.style.opacity = opacity
+      }
+    })
+    logo.style.opacity = opacity
+  }
+}
+
+nav.addEventListener('mouseover', (e) => {
+  handleHover(e, 0.5)
+})
+
+nav.addEventListener('mouseout', (e) => {
+  handleHover(e, 1)
+})
+
+//////////////////////////////////////////////////
+// ! STICKY NAVIGATION
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords)
+// window.addEventListener('scroll', (e) => { 
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky')
+//   else nav.classList.remove('sticky')
+// })
+// USING INTERSECTION OBSERVER
+const obsCallback = (entries, observer) => { 
+  entries.forEach(entry => {
+    console.log(entry)
+  })
+}
+
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2],
+}
+
+const observer = new IntersectionObserver(obsCallback, obsOptions)
+observer.observe(section1)
